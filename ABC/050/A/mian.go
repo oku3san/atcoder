088efba3dd1d5e4141c67814174e2bab3df85f2c
oku3san <takaya0303@gmail.com>
2022-06-13 00:14:19 +0900

    ABC050A

---
+++ ABC/050/A/mian.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a, b int
+  var op string
+  fmt.Scan(&a, &op, &b)
+
+  switch op {
+  case "+":
+    fmt.Println(a + b)
+  case "-":
+    fmt.Println(a - b)
+  }
+}
