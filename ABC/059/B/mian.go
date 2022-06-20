145aba7f937b6fe568959844c33e08827b0dfc66
oku3san <takaya0303@gmail.com>
2022-06-20 23:46:38 +0900

    ABC059B-v2

--- ABC/059/B/mian.go
+++ ABC/059/B/mian.go
@@ -1,16 +1,32 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math/big"
+)
 
 func main() {
-  var A, B float64
-  fmt.Scan(&A, &B)
-  switch {
-  case A == B:
+  //var A, B float64
+  //fmt.Scan(&A, &B)
+  //switch {
+  //case A == B:
+  //  fmt.Println("EQUAL")
+  //case A > B:
+  //  fmt.Println("GREATER")
+  //case A < B:
+  //  fmt.Println("LESS")
+  //}
+
+  var A = new(big.Int)
+  var B = new(big.Int)
+  fmt.Scan(A, B)
+  switch A.Cmp(B) {
+  case 0:
     fmt.Println("EQUAL")
-  case A > B:
+  case 1:
     fmt.Println("GREATER")
-  case A < B:
+  case -1:
     fmt.Println("LESS")
   }
+
 }
