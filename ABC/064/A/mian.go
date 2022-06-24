0192188dade948d9e142cbe4b64d5045c1773f8b
oku3san <takaya0303@gmail.com>
2022-06-25 01:54:38 +0900

    ABC064A-2

--- ABC/064/A/mian.go
+++ ABC/064/A/mian.go
@@ -1,14 +1,28 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strconv"
+)
 
 func main() {
-  var r, g, b int
+  //var r, g, b int
+  //fmt.Scan(&r, &g, &b)
+  //
+  //score := (r * 100) + (g * 10) + (b * 1)
+  //
+  //if score%4 == 0 {
+  //  fmt.Println("YES")
+  //} else {
+  //  fmt.Println("NO")
+  //}
+
+  var r, g, b string
   fmt.Scan(&r, &g, &b)
 
-  score := (r * 100) + (g * 10) + (b * 1)
+  i, _ := strconv.Atoi(r + g + b)
 
-  if score%4 == 0 {
+  if i%4 == 0 {
     fmt.Println("YES")
   } else {
     fmt.Println("NO")
