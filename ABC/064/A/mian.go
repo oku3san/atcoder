d2fe1303f5ea3888db854bd72e152c443b10d8bb
oku3san <takaya0303@gmail.com>
2022-06-25 01:52:35 +0900

    ABC064A

---
+++ ABC/064/A/mian.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  var r, g, b int
+  fmt.Scan(&r, &g, &b)
+
+  score := (r * 100) + (g * 10) + (b * 1)
+
+  if score%4 == 0 {
+    fmt.Println("YES")
+  } else {
+    fmt.Println("NO")
+  }
+}
